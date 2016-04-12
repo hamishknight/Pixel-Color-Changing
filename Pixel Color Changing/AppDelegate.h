d41712b6e9be0ab0d5a3f540acf92bfaa8c10ccb
Hamish Knight <hk@southerncare.co.uk>
2016-04-12 18:48:22 +0100

    Initial Commit

---
+++ Pixel Color Changing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pixel Color Changing
+//
+//  Created by Hamish Knight on 12/04/2016.
+//  Copyright © 2016 Redonkulous Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pixel Color Changing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pixel Color Changing
+//
+//  Created by Hamish Knight on 12/04/2016.
+//  Copyright © 2016 Redonkulous Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
